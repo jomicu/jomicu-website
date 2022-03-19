440a07c6dfaad0e298faff395254cb31ed49ad0e
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-03-19 18:54:05 +0000

    a lot of things

---
+++ infrastructure/website/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.1"
+    }
+  }
+
+  backend "s3" {
+    bucket = "AWS_TERRAFORM_BUCKET_PLACEHOLDER"
+    key    = "ENVIRONMENT_PLACEHOLDER/SERVICE_PLACEHOLDER/terraform.tfstate"
+    region = "AWS_REGION_PLACEHOLDER"
+    acl    = "bucket-owner-full-control"
+  }
+}
+
+provider "aws" {
+  region     = "AWS_REGION_PLACEHOLDER"
+}
+
+module "configuration" {
+  source       = "SOURCE_PLACEHOLDER"
+  environment  = "ENVIRONMENT_PLACEHOLDER"
+  service_name = "SERVICE_PLACEHOLDER"
+}
