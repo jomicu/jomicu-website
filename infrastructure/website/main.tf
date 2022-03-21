765d24f5f84335c7d068cbb905fd66ff9747b8de
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-03-21 19:58:24 +0000

    Update main.tf

--- infrastructure/website/main.tf
+++ infrastructure/website/main.tf
@@ -20,6 +20,9 @@ provider "aws" {
 
 module "configuration" {
   source       = "MODULE_SOURCE_PLACEHOLDER"
+  aws_region   = "AWS_REGION_PLACEHOLDER"
   environment  = "ENVIRONMENT_PLACEHOLDER"
   service_name = "SERVICE_PLACEHOLDER"
+  domain_name  = "DOMAIN_NAME_PLACEHOLDER"
+  endpoint     = "ENDPOINT_PLACEHOLDER"
 }
