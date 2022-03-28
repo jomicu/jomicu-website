e01ffc81734e9c05eca5b4b09305e3b7b8da2e6e
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-03-28 22:25:03 +0100

    Revert "Removed providers (#11)"
    This reverts commit b29c00a5f05fcfda071dc883599718960255d152.

--- infrastructure/website/main.tf
+++ infrastructure/website/main.tf
@@ -7,9 +7,12 @@ terraform {
   }
 }
 
+provider "aws" {
+    region = "__AWS_REGION_PLACEHOLDER__"
+}
+
 module "configuration" {
   source           = "__MODULE_SOURCE_PLACEHOLDER__"
-  aws_region       = "__AWS_REGION_PLACEHOLDER__"
   deployed_service = "__SERVICE_PLACEHOLDER__"
   environment      = "__ENVIRONMENT_PLACEHOLDER__"
   domain           = "__DOMAIN_PLACEHOLDER__"
