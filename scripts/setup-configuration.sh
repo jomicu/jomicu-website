6a28abd44ca45732d549c4aff46175db84e74305
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-03-28 21:54:45 +0100

    Refactored infrastructure (#7)

--- scripts/setup-configuration.sh
+++ scripts/setup-configuration.sh
@@ -1,13 +1,26 @@
-# This script will replace the placeholders in the main.tf file with the correct values
+# This script will replace the placeholders with the correct values
+
+# Placeholders defined in main.tf
+AWS_REGION_PLACEHOLDER="__AWS_REGION_PLACEHOLDER__"
+TERRAFORM_STATE_FILE_NAME_PLACEHOLDER="__TERRAFORM_STATE_FILE_NAME_PLACEHOLDER__"
+TERRAFORM_BUCKET_NAME_PLACEHOLDER="__TERRAFORM_BUCKET_NAME_PLACEHOLDER__"
+SERVICE_PLACEHOLDER="__SERVICE_PLACEHOLDER__"
+MODULE_SOURCE_PLACEHOLDER="__MODULE_SOURCE_PLACEHOLDER__"
+ENVIRONMENT_PLACEHOLDER="__ENVIRONMENT_PLACEHOLDER__"
+DOMAINS_PLACEHOLDER="__DOMAINS_PLACEHOLDER__"
+SUBDOMAIN_PLACEHOLDER="__SUBDOMAINS_PLACEHOLDER__"
+CACHE_TTL_PLACEHOLDER="__CACHE_TTL_PLACEHOLDER__"
 
 # Standard configuration
-AWS_TERRAFORM_BUCKET="jomicu-terraform-state"
-MODULE_SOURCE="git@github.com:jomicu/terraform-aws-website-module.git//module?ref=${ENVIRONMENT}"
+TERRAFORM_STATE_FILE_NAME="terraform.tfstate"
+TERRAFORM_BUCKET_NAME="jomicu-terraform-state"
 SERVICE="jomicu-website"
+MODULE_SOURCE="git@github.com:AWS-Infrastructure-Modules/terraform-static-website.git//module"
+
+# Website domain
 DOMAIN="jomicu.com"
 
-# The endpoint will be dependent on the environment
-# Cache will depend on the environment
+# The endpoint and cache will be dependent on the environment
 if [ "${ENVIRONMENT}" = "production" ]
 then
     SUBDOMAIN="www"
@@ -17,15 +30,19 @@ else
     CACHE_TTL="0"
 fi
 
-# main.tf path to replace values
-TERRAFORM_MAIN="../infrastructure/website/main.tf"
+# Path to main.tf
+PATH_TO_INFRASTRUCTURE="../infrastructure/website"
+FILE_WITH_PLACEHOLDERS="main.tf"
+PATH_TO_PLACEHOLDERS="${PATH_TO_INFRASTRUCTURE}/${FILE_WITH_PLACEHOLDERS}"
 
-sed -i "s|AWS_REGION_PLACEHOLDER|$AWS_REGION|g" "${TERRAFORM_MAIN}"
-sed -i "s|AWS_TERRAFORM_BUCKET_PLACEHOLDER|$AWS_TERRAFORM_BUCKET|g" "${TERRAFORM_MAIN}"
-sed -i "s|MODULE_SOURCE_PLACEHOLDER|$MODULE_SOURCE|g" "${TERRAFORM_MAIN}"
-sed -i "s|SERVICE_PLACEHOLDER|$SERVICE|g" "${TERRAFORM_MAIN}"
-sed -i "s|ENVIRONMENT_PLACEHOLDER|$ENVIRONMENT|g" "${TERRAFORM_MAIN}"
-sed -i "s|SUBDOMAIN_PLACEHOLDER|$SUBDOMAIN|g" "${TERRAFORM_MAIN}"
-sed -i "s|DOMAIN_PLACEHOLDER|$DOMAIN|g" "${TERRAFORM_MAIN}"
-sed -i "s|CACHE_TTL_PLACEHOLDER|$CACHE_TTL|g" "${TERRAFORM_MAIN}"
+# Replace placeholders with actual values
+sed -i "s|${AWS_REGION_PLACEHOLDER}|${AWS_REGION}|g" "${PATH_TO_PLACEHOLDERS}"
+sed -i "s|${TERRAFORM_STATE_FILE_NAME_PLACEHOLDER}|${TERRAFORM_STATE_FILE_NAME}|g" "${PATH_TO_PLACEHOLDERS}"
+sed -i "s|${TERRAFORM_BUCKET_NAME_PLACEHOLDER}|${TERRAFORM_BUCKET_NAME}|g" "${PATH_TO_PLACEHOLDERS}"
+sed -i "s|${SERVICE_PLACEHOLDER}|${SERVICE}|g" "${PATH_TO_PLACEHOLDERS}"
+sed -i "s|${MODULE_SOURCE_PLACEHOLDER}|${MODULE_SOURCE}|g" "${PATH_TO_PLACEHOLDERS}"
+sed -i "s|${ENVIRONMENT_PLACEHOLDER}|${ENVIRONMENT}|g" "${PATH_TO_PLACEHOLDERS}"
+sed -i "s|${DOMAIN_PLACEHOLDER}|${DOMAIN}|g" "${PATH_TO_PLACEHOLDERS}"
+sed -i "s|${SUBDOMAIN_PLACEHOLDER}|${SUBDOMAIN}|g" "${PATH_TO_PLACEHOLDERS}"
+sed -i "s|${CACHE_TTL_PLACEHOLDER}|${CACHE_TTL}|g" "${PATH_TO_PLACEHOLDERS}"
 
